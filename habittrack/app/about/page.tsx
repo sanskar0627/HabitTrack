3a636fe73ef46f236e46d319098e2fb7bee1ef84
Sanskar Shukla <sanskar0627@gmail.com>
2025-08-10 10:59:41 +0530

    Implemented Next Auth

--- habittrack/app/about/page.tsx
+++ habittrack/app/about/page.tsx
@@ -3,6 +3,7 @@ export default function AboutPage() {
     <main>
       <h1>About</h1>
       <p>This is the About page of our habit tracker.</p>
+      <p>This is the Sanskar Shukla.</p>
     </main>
   );
 }
