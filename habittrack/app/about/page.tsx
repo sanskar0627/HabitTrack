d82664880a17fbe2f1d474113143ad148b6510f0
Sanskar Shukla <sanskar0627@gmail.com>
2025-08-13 17:29:45 +0530

    deployment

--- habittrack/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>About</h1>
-      <p>This is the About page of our habit tracker.</p>
-      <p>This is the Sanskar Shukla.</p>
-    </main>
-  );
-}
